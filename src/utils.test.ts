dd38e5abe7060bee03f0538e60bd8bb385db2bc7
ahm0xc <codingwebsa@gmail.com>
2025-05-01 03:19:32 +0600

    run fmt

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -3,8 +3,8 @@ import { test, expect } from "vitest";
 import { delay } from "./utils.js";
 
 test("delay", async () => {
-    const start = Date.now();
-    await delay(10);
-    const end = Date.now();
-    expect(end - start).toBeGreaterThanOrEqual(10);
+  const start = Date.now();
+  await delay(10);
+  const end = Date.now();
+  expect(end - start).toBeGreaterThanOrEqual(10);
 });
