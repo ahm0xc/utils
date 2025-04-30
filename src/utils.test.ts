08f9ed8c5a985d84454b454b55573bea54b659c9
ahm0xc <codingwebsa@gmail.com>
2025-05-01 03:17:29 +0600

    setup vitest

---
+++ src/utils.test.ts
@@ -0,0 +1,10 @@
+import { test, expect } from "vitest";
+
+import { delay } from "./utils.js";
+
+test("delay", async () => {
+    const start = Date.now();
+    await delay(10);
+    const end = Date.now();
+    expect(end - start).toBeGreaterThanOrEqual(10);
+});
